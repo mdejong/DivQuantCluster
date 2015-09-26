255e2d83fe8e4c2cb8097c0e17f16f57ca0c4558
Mo DeJong <mo@modejong.com>
2015-09-26 16:59:43 -0700

    cleanup some code in DivQuantMapColors and add defines for debugging of map_colors_mps, no functional changes

--- DivQuant/DivQuantMapColors.cpp
+++ DivQuant/DivQuantMapColors.cpp
@@ -15,8 +15,6 @@
 
 #include "DivQuantHeader.h"
 
-#include <stdlib.h>
-
 #include <assert.h>
 
 #define L2_SQR( X1, Y1, Z1, X2, Y2, Z2 )\
@@ -26,10 +24,6 @@ temp = ( Y1 ) - ( Y2 );\
 dist += temp * temp;\
 temp = ( Z1 ) - ( Z2 );\
 dist += temp * temp\
-
-#define L2_SQR_VEC( VEC_A, VEC_B )\
-L2_SQR ( VEC_A[0], VEC_A[1], VEC_A[2],\
-VEC_B[0], VEC_B[1], VEC_B[2] )\
 
 static
 int
@@ -45,7 +39,6 @@ L2_sqr_int ( const int x1, const int y1, const int z1,
 }
 
 #undef L2_SQR
-#undef L2_SQR_VEC
 
 void
 check_mem ( const int x )
@@ -226,17 +219,26 @@ get_double_scale(
   return weight;
 }
 
-int compare_Pixel_Int( const void * l, const void * r) {
-  Pixel_Int *left = (Pixel_Int *) l;
-  Pixel_Int *right= (Pixel_Int *) r;
-  return ( left->weight - right->weight );
+static inline
+bool asc_weighted_pixel(const Pixel_Int & a, const Pixel_Int & b) {
+  return a.weight < b.weight;
 }
 
 static void
 sort_color ( Pixel_Int *cmap, const int num_colors )
 {
-  qsort ( cmap, num_colors, sizeof(Pixel_Int), compare_Pixel_Int);
+  std::vector<Pixel_Int> pixelVec(num_colors);
+  for ( int i = 0; i < num_colors; i++ ) {
+    pixelVec[i] = cmap[i];
+  }
+  std::sort(begin(pixelVec), end(pixelVec), asc_weighted_pixel);
+  for ( int i = 0; i < num_colors; i++ ) {
+    cmap[i] = pixelVec[i];
+  }
 }
+
+//#define SEARCH_DEBUG
+//#define SEARCH_DEBUG_SORT
 
 void
 map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outPixelsPtr, uint32_t *outColortablePtr, int colormapSize )
@@ -265,9 +267,6 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
   lut_init = ( int * ) malloc ( size_lut_init * sizeof ( int ) );
   check_mem ( lut_init == NULL );
   
-  /* TODO */
-  /* Duplicate the color map */
-  
   cmap = ( Pixel_Int * ) malloc ( num_colors * sizeof ( Pixel_Int ) );
   for (int i = 0; i < num_colors; i++) {
     uint32_t pixel = outColortablePtr[i];
@@ -278,9 +277,11 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
     pi->weight = 0;
   }
   
-  //  for ( int si = 0; si < num_colors; si++ ) {
-  //    printf("table[%3d] = (%3d %3d %3d) 0x%02X%02X%02X\n", si, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].red, cmap[si].green, cmap[si].blue);
-  //  }
+#if defined(SEARCH_DEBUG_SORT)
+  for ( int si = 0; si < num_colors; si++ ) {
+    printf("table[%3d] = (%3d %3d %3d) 0x%02X%02X%02X\n", si, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].red, cmap[si].green, cmap[si].blue);
+  }
+#endif // SEARCH_DEBUG_SORT
   
   size_lut_ssd = 2 * max_sum + 1;
   lut_ssd_buffer = ( double * ) malloc ( size_lut_ssd * sizeof ( double ) );
@@ -289,7 +290,7 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
   lut_ssd = lut_ssd_buffer + max_sum;
   lut_ssd[0] = 0;
   
-  /* Premultiply the LUT entries by (1/3) -- see below */
+  // Premultiply the LUT entries by (1/3) -- see below
   for ( ik = 1; ik <= max_sum; ik++ )
   {
 #if defined(DEBUG)
@@ -310,7 +311,7 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
     lut_ssd[-ik] = lut_ssd[ik] = ( ik * ik ) / 3.0;
   }
   
-  /* Sort the palette by the sum of color components. */
+  // Sort the palette by the sum of color components.
   for ( ic = 0; ic < num_colors; ic++ )
   {
 #if defined(DEBUG)
@@ -322,9 +323,11 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
   
   sort_color ( cmap, num_colors );
   
-  //  for ( int si = 0; si < num_colors; si++ ) {
-  //    printf("table[%3d] = (%3d %3d %3d) 0x%02X%02X%02X : w %3d\n", si, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].weight);
-  //  }
+#if defined(SEARCH_DEBUG_SORT)
+  for ( int si = 0; si < num_colors; si++ ) {
+    printf("table[%3d] = (%3d %3d %3d) 0x%02X%02X%02X : w %3d\n", si, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].weight);
+  }
+#endif // SEARCH_DEBUG_SORT
   
   // Calculate the LUT
   if (num_colors >= 2) {
@@ -349,7 +352,7 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
 #if defined(DEBUG)
     assert((num_colors-2) >= 0);
 #endif // DEBUG
-    high = ( int ) ( 0.5 * ( cmap[num_colors - 2].weight + cmap[num_colors - 1].weight ) + 0.5 ); /* round */
+    high = ( int ) ( 0.5 * ( cmap[num_colors - 2].weight + cmap[num_colors - 1].weight ) + 0.5 ); // round
   } else {
     high = 1;
   }
@@ -368,8 +371,8 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
     assert(((ic + 1) >= 0) && ((ic + 1) < num_colors));
 #endif // DEBUG
     
-    low = ( int ) ( 0.5 * ( cmap[ic - 1].weight + cmap[ic].weight ) + 0.5 );  /* round */
-    high = ( int ) ( 0.5 * ( cmap[ic].weight + cmap[ic + 1].weight ) + 0.5 ); /* round */
+    low = ( int ) ( 0.5 * ( cmap[ic - 1].weight + cmap[ic].weight ) + 0.5 );  // round
+    high = ( int ) ( 0.5 * ( cmap[ic].weight + cmap[ic + 1].weight ) + 0.5 ); // round
     
     for ( ik = low; ik < high; ik++ )
     {
@@ -392,18 +395,30 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
     //      printf("table[%3d] = (%3d %3d %3d) 0x%02X%02X%02X : w = %d\n", si, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].red, cmap[si].green, cmap[si].blue, cmap[si].weight);
     //    }
     
-    /* Determine the initial searched colour cinit in the palette for cp. */
+    // Determine the initial searched colour cinit in the palette for cp.
 #if defined(DEBUG)
     assert(sum >= 0);
     assert(sum < size_lut_init);
 #endif // DEBUG
     index = lut_init[sum];
     
-    //    printf("L2 search start at index %d\n", index);
+#if defined(SEARCH_DEBUG)
+    printf("L2 search start at index %d for pixel 0x%08X : (%3d %3d %3d)\n", index, pixel, red, green, blue);
+#endif // SEARCH_DEBUG
     
-    /* Calculate the squared Euclidean distance between cp and cinit */
+    // Calculate the squared Euclidean distance between cp and cinit
     min_dist = L2_sqr_int ( red, green, blue,
                            cmap[index].red, cmap[index].green, cmap[index].blue );
+    
+#if defined(SEARCH_DEBUG)
+    {
+      B = ( uint8_t ) cmap[index].blue;
+      G = ( uint8_t ) cmap[index].green;
+      R = ( uint8_t ) cmap[index].red;
+      uint32_t tablePixel = (R << 16) | (G << 8) | B;
+      printf("min_dist from 0x%08X (%3d %3d %3d) to 0x%08X (%3d %3d %3d) (at index %d) is %d\n", pixel, red, green, blue, index, tablePixel, cmap[index].red, cmap[index].green, cmap[index].blue, min_dist);
+    }
+#endif // SEARCH_DEBUG
     
     m = n = index;
     up = down = 1;
@@ -415,19 +430,30 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
         
         if ( ( m > ( num_colors - 1 ) ) || ( lut_ssd[sum - cmap[m].weight] > min_dist ) )
         {
-          /* Terminate the search in DOWN direction */
+          // Terminate the search in DOWN direction
           down = 0;
+          
+#if defined(SEARCH_DEBUG)
+          printf("terminate down search at low index %3d\n", m);
+#endif // SEARCH_DEBUG
         }
         else
         {
           dist = L2_sqr_int ( red, green, blue,
                              cmap[m].red, cmap[m].green, cmap[m].blue );
-          //    printf("L2 (1) from (%d %d %d) to 0x%02X%02X%02X (%d %d %d) (index %d) = %d\n", red, green, blue, cmap[m].red, cmap[m].green, cmap[m].blue, cmap[m].red, cmap[m].green, cmap[m].blue, m, dist);
+          
+#if defined(SEARCH_DEBUG)
+          printf("L2 (down) at %3d from (%d %d %d) to 0x%02X%02X%02X (%d %d %d) = %d\n", m, red, green, blue, cmap[m].red, cmap[m].green, cmap[m].blue, cmap[m].red, cmap[m].green, cmap[m].blue, dist);
+#endif // SEARCH_DEBUG
           
           if ( dist < min_dist )
           {
             min_dist = dist;
             index = m;
+            
+#if defined(SEARCH_DEBUG)
+            printf("L2 (down) new min at %d\n", index);
+#endif // SEARCH_DEBUG
           }
         }
       }
@@ -438,31 +464,44 @@ map_colors_mps ( const uint32_t *inPixelsPtr, uint32_t numPixels, uint32_t *outP
         
         if ( ( n < 0 ) || ( lut_ssd[sum - cmap[n].weight] > min_dist ) )
         {
-          /* Terminate the search in UP direction */
+          // Terminate the search in UP direction
           up = 0;
+          
+#if defined(SEARCH_DEBUG)
+          printf("terminate up search at high index %3d\n", n);
+#endif // SEARCH_DEBUG
         }
         else
         {
           dist = L2_sqr_int ( red, green, blue,
                              cmap[n].red, cmap[n].green, cmap[n].blue );
-          //    printf("L2 (2) from (%d %d %d) to 0x%02X%02X%02X (%d %d %d) (index %d) = %d\n", red, green, blue, cmap[m].red, cmap[m].green, cmap[m].blue, cmap[m].red, cmap[m].green, cmap[m].blue, m, dist);
           
-          
+#if defined(SEARCH_DEBUG)
+          printf("L2 (up  ) at %3d from (%d %d %d) to 0x%02X%02X%02X (%d %d %d) = %d\n", n, red, green, blue, cmap[n].red, cmap[n].green, cmap[n].blue, cmap[n].red, cmap[n].green, cmap[n].blue, dist);
+#endif // SEARCH_DEBUG
           
           if ( dist < min_dist ) 
           { 
             min_dist = dist; 
             index = n;
+            
+#if defined(SEARCH_DEBUG)
+            printf("L2 (up) new min at %d\n", index);
+#endif // SEARCH_DEBUG
           }
         }
       }
     }
     
-    B = ( uchar ) cmap[index].blue;
-    G = ( uchar ) cmap[index].green;
-    R = ( uchar ) cmap[index].red;
+    B = ( uint8_t ) cmap[index].blue;
+    G = ( uint8_t ) cmap[index].green;
+    R = ( uint8_t ) cmap[index].red;
     pixel = (R << 16) | (G << 8) | B;
     outPixelsPtr[ik] = pixel;
+    
+#if defined(SEARCH_DEBUG)
+    printf("L2 search finished on index %3d : pixel 0x%08X : (%d %d %d) and min_dist %d\n", index, pixel, R, G, B, min_dist);
+#endif // SEARCH_DEBUG
   }
   
   free ( lut_init );
